411835a0c2948777f00dc3ffa82fb80de3fb3e2f
naspitha <naspitha@users.noreply.github.com>
2023-12-01 17:52:13 +0100

    some new words

--- files/csv2json.py
+++ files/csv2json.py
@@ -14,9 +14,16 @@ with open(csv_file, mode='r', encoding='utf-8') as csv_file:
     json_dictionary['data'] = []
     for r, row_data in enumerate(csvfiledata):
         fixed_data = {'Kana' if k == '\ufeffKana' else k:v for k,v in row_data.items()}
-        fixed_data['id']=r
+        word = fixed_data
+        s = "-".join([word["Kana"], word["English"][:15], word["Category"]])
+        swaps = [("{", "X"), ("}", "X"), ("/", "L")]
+        for old, new in swaps:
+            s = s.replace(old, new)
+        fixed_data['id']=s
         # print(fixed_data)
         json_dictionary['data'].append(fixed_data)
 # print(json_dictionary)
 with open(json_file, 'w') as json_file:
     json_file.write(json.dumps(json_dictionary, indent=4))
+
+    
